2dfa989ea2d5df62626a05a5e098a0f219f6f2aa
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-24 00:10:03 +0900

    表示コンポーネントの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import theme from "./theme/theme";
 import { Button, ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
+import theme from "./theme/theme";
 
 export default function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button colorScheme="teal">ボタン</Button>
-      <p>ああああ</p>
+      <BrowserRouter>
+        <Button colorScheme="teal">ボタン</Button>
+        <p>ああああ</p>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
