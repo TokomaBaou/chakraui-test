c51849ffbf424fa790700dd66ecbf5eeb3a2d44b
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-24 14:41:22 +0900

    useDisclosure ハンバーガーメニューの追加と、メニューの表示

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,5 +1,69 @@
 import { memo, VFC } from "react";
+import {
+  Flex,
+  Heading,
+  Link,
+  Box,
+  IconButton,
+  Drawer,
+  DrawerBody,
+  DrawerOverlay,
+  Button,
+  useDisclosure,
+  DrawerContent
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 export const Header: VFC = memo(() => {
-  return <div style={{ height: "50px", backgroundColor: "teal" }}></div>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      {/* //Flexboxを使用出来るタグ
+    //asを付けることで、どのタグにレンダリングするのか決める事が出来る */}
+      <Flex
+        as="nav"
+        bg="teal.500"
+        color="gray.50"
+        align="center"
+        justify="space-between"
+        padding={{ base: 3, md: 5 }}
+      >
+        <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+            ユーザー管理アプリ
+          </Heading>
+        </Flex>
+        <Flex
+          align="center"
+          fontSize="sm"
+          flexGrow={2}
+          display={{ base: "none", md: "flex" }}
+        >
+          <Box pr={4}>
+            <Link>ユーザー一覧</Link>
+          </Box>
+          <Link>設定</Link>
+        </Flex>
+        <IconButton
+          aria-label="メニューボタン"
+          icon={<HamburgerIcon />}
+          size="sm"
+          variant="unstyled"
+          display={{ base: "block", md: "none" }}
+          onClick={onOpen}
+        />
+      </Flex>
+      <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerBody p={0} bg="gray.100">
+              <Button w="100%">TOP</Button>
+              <Button w="100%">ユーザー一覧</Button>
+              <Button w="100%">設定</Button>
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </>
+  );
 });
