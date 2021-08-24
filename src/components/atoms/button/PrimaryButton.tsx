e3779c25ce90a6cc46fa3196048cb2f0641b192a
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-24 17:15:19 +0900

    PrimaryButton,Login ログインボタンをコンポーネント化して、PrimaryButtonを生成した。

---
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -0,0 +1,17 @@
+import { memo, ReactNode, VFC } from "react";
+
+import { Button } from "@chakra-ui/react";
+
+//propsとしては、ボタンの名称を受け取れたら良い
+type Props = {
+  children: ReactNode;
+};
+
+export const PrimaryButton: VFC<Props> = memo((props) => {
+  const { children } = props;
+  return (
+    <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+      {children}
+    </Button>
+  );
+});
