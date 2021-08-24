95578a6c9d605a8e7a99d399e9599c0bce705135
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-24 12:39:07 +0900

    header.HeaderLayout ヘッダーの生成とヘッダーレイアウトの管理

--- src/router/HomeRoutes.tsx
+++ src/router/HomeRoutes.tsx
@@ -1,6 +1,7 @@
 import { Home } from "../components/pages/Home";
 import { Setting } from "../components/pages/Setting";
 import { UserManagement } from "../components/pages/UserManagement";
+import { Page404 } from "../components/pages/404";
 
 export const homeRoutes = [
   {
@@ -17,5 +18,11 @@ export const homeRoutes = [
     path: "/setting",
     exact: false,
     children: <Setting />
+  },
+  // home/*を設定する
+  {
+    path: "*",
+    exact: false,
+    children: <Page404 />
   }
 ];
