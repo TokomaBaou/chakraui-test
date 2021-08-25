4da5d693ffedb09c5d21eb1dff756b8d44fab14f
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-25 17:43:21 +0900

    userDetailModalにisAdminフラグを渡す

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -20,7 +20,7 @@ export const UserManagement: VFC = memo(() => {
   const { onSelectUser, selectedUser } = useSelectUser();
 
   const { loginUser } = useLoginUser();
-  console.log(loginUser);
+  // console.log(loginUser);
 
   useEffect(() => getUsers(), []);
 
@@ -54,7 +54,12 @@ export const UserManagement: VFC = memo(() => {
           ))}
         </Wrap>
       )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal
+        user={selectedUser}
+        isOpen={isOpen}
+        onClose={onClose}
+        isAdmin={loginUser?.isAdmin}
+      />
     </>
   );
 });
