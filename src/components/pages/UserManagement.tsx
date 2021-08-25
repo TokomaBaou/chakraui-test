30162f6060a84e87fb83ac571721a2faeb29bc4a
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-25 10:53:24 +0900

    userManagement userManagementにてユーザー一覧画面　　　　　　　　　の生成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,36 @@
+import { Box, Stack, Wrap, WrapItem, Image, Text } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページです</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <Box
+          w="260px"
+          h="260px"
+          bg="white"
+          borderRadius="10px"
+          shadow="md"
+          p={4}
+          _hover={{ cursor: "pointer", opacity: 0.8 }}
+        >
+          <Stack textAlign="center">
+            <Image
+              borderRadius="full"
+              boxSize="160px"
+              m="auto"
+              alt="プロフィール画像"
+              src="https://source.unsplash.com/random"
+            />
+            <Text fontSize="lg" fontWeight="bold">
+              マドカダイゴ
+            </Text>
+            <Text fontSize="sm" color="gray">
+              光の巨人
+            </Text>
+          </Stack>
+        </Box>
+      </WrapItem>
+    </Wrap>
+  );
 });
