91630e142c8c911d9923effbf7d1660aae939d6a
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-25 17:36:16 +0900

    user の値でstateを更新 userDetailModalでuserのログイン情報次第でstateの値を更新するように設定

--- src/components/organisms/user/UserDetailModal.tsx
+++ src/components/organisms/user/UserDetailModal.tsx
@@ -8,20 +8,51 @@ import {
   ModalBody,
   FormControl,
   FormLabel,
-  Input
+  Input,
+  ModalFooter
 } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { ChangeEvent, memo, useEffect, useState, VFC } from "react";
 import { User } from "../../../types/api/user";
+import { PrimaryButton } from "../../atoms/button/PrimaryButton";
 
 //propsとしては、ボタンの名称を受け取れたら良い
 type Props = {
   user: User | null;
   isOpen: boolean;
   onClose: () => void;
+  //管理者導線を追加
+  isAdmin?: boolean;
 };
 
 export const UserDetailModal: VFC<Props> = memo((props) => {
-  const { user, isOpen, onClose } = props;
+  const { user, isOpen, onClose, isAdmin = false } = props;
+
+  //valueの値をstate経由で渡す
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  //userの値でstateを更新する
+  useEffect(() => {
+    setUsername(user?.username ?? "");
+    setName(user?.name ?? "");
+    setEmail(user?.email ?? "");
+    setPhone(user?.phone ?? "");
+  }, [user]);
+
+  //管理者の場合に変更　onChange
+  const onChangeUserName = (event: ChangeEvent<HTMLInputElement>) =>
+    setUsername(event.target.value);
+  const onChangeName = (event: ChangeEvent<HTMLInputElement>) =>
+    setName(event.target.value);
+  const onChangeEmail = (event: ChangeEvent<HTMLInputElement>) =>
+    setEmail(event.target.value);
+  const onChangePhone = (event: ChangeEvent<HTMLInputElement>) =>
+    setPhone(event.target.value);
+
+  const onClickUpdate = () => alert();
+
   return (
     <Modal
       isOpen={isOpen}
@@ -30,29 +61,52 @@ export const UserDetailModal: VFC<Props> = memo((props) => {
       motionPreset="slideInBottom"
     >
       <ModalOverlay />
-      <ModalContent pb={6}>
+      <ModalContent pb={2}>
         <ModalHeader>ユーザー詳細</ModalHeader>
         <ModalCloseButton />
         <ModalBody mx={4}>
           <Stack spacing={4}>
             <FormControl>
               <FormLabel>名前</FormLabel>
-              <Input value={user?.username} isReadOnly />
+              {/* <Input value={user?.username} isReadOnly={!isAdmin} />
+               */}
+              <Input
+                value={username}
+                isReadOnly={!isAdmin}
+                onChange={onChangeUserName}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>フルネーム</FormLabel>
-              <Input value={user?.name} isReadOnly />
+              <Input
+                value={name}
+                isReadOnly={!isAdmin}
+                onChange={onChangeName}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>MAIL</FormLabel>
-              <Input value={user?.email} isReadOnly />
+              <Input
+                value={email}
+                isReadOnly={!isAdmin}
+                onChange={onChangeEmail}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>TEL</FormLabel>
-              <Input value={user?.phone} isReadOnly />
+              <Input
+                value={phone}
+                isReadOnly={!isAdmin}
+                onChange={onChangePhone}
+              />
             </FormControl>
           </Stack>
         </ModalBody>
+        {isAdmin && (
+          <ModalFooter>
+            <PrimaryButton onClick={onClickUpdate}>更新</PrimaryButton>
+          </ModalFooter>
+        )}
       </ModalContent>
     </Modal>
   );
