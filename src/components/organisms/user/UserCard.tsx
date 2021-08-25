ac7e290813e487de0bfa62e69f2f1992d858932d
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-25 11:11:28 +0900

    userCard,userMnagement user情報のカード部分をコンポーネント分割した。

---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,40 @@
+import { Box, Stack, Image, Text } from "@chakra-ui/react";
+import { memo, VFC } from "react";
+
+//propsとしては、ボタンの名称を受け取れたら良い
+type Props = {
+  imageUrl: string;
+  userName: string;
+  fullName: string;
+};
+
+export const UserCard: VFC<Props> = memo((props) => {
+  const { imageUrl, userName, fullName } = props;
+  return (
+    <Box
+      w="260px"
+      h="260px"
+      bg="white"
+      borderRadius="10px"
+      shadow="md"
+      p={4}
+      _hover={{ cursor: "pointer", opacity: 0.8 }}
+    >
+      <Stack textAlign="center">
+        <Image
+          borderRadius="full"
+          boxSize="160px"
+          m="auto"
+          alt={userName}
+          src={imageUrl}
+        />
+        <Text fontSize="lg" fontWeight="bold">
+          {userName}
+        </Text>
+        <Text fontSize="sm" color="gray">
+          {fullName}
+        </Text>
+      </Stack>
+    </Box>
+  );
+});
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,35 +1,16 @@
-import { Box, Stack, Wrap, WrapItem, Image, Text } from "@chakra-ui/react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
   return (
     <Wrap p={{ base: 4, md: 10 }}>
       <WrapItem>
-        <Box
-          w="260px"
-          h="260px"
-          bg="white"
-          borderRadius="10px"
-          shadow="md"
-          p={4}
-          _hover={{ cursor: "pointer", opacity: 0.8 }}
-        >
-          <Stack textAlign="center">
-            <Image
-              borderRadius="full"
-              boxSize="160px"
-              m="auto"
-              alt="プロフィール画像"
-              src="https://source.unsplash.com/random"
-            />
-            <Text fontSize="lg" fontWeight="bold">
-              マドカダイゴ
-            </Text>
-            <Text fontSize="sm" color="gray">
-              光の巨人
-            </Text>
-          </Stack>
-        </Box>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="マドカダイゴ"
+          fullName="光の巨人"
+        />
       </WrapItem>
     </Wrap>
   );
